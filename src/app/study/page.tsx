d7588d31bc075d9b0c8325cb03469d6bccf83464
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2025-04-03 20:40:18 +0900

    add making study group page

--- src/app/study/page.tsx
+++ src/app/study/page.tsx
@@ -4,8 +4,10 @@ import { Box, Typography } from "@mui/material";
 
 export default function Study() {
   return (
-    <Box>
-      <Typography variant="h1">Study</Typography>
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Typography variant="h4">study作成</Typography>
     </Box>
   );
 }
