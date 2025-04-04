5c5b619ddc280770e8e7a2550ee51001e7733256
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2025-04-04 18:56:33 +0900

    add form layout

--- src/app/study/page.tsx
+++ src/app/study/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 export default function Study() {
   return (
@@ -8,6 +8,16 @@ export default function Study() {
       sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <Typography variant="h4">study作成</Typography>
+      <Box
+        component="form"
+        sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+      >
+        <TextField name="title" label="タイトル" />
+        <TextField name="description" label="説明" />
+        <Button type="submit" variant="contained" color="primary">
+          作成
+        </Button>
+      </Box>
     </Box>
   );
 }
