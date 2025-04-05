e353194912303f4c0d39740f4f61fccbf20d26b5
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2025-04-05 10:25:10 +0900

    add toggleBtn

--- src/app/study/page.tsx
+++ src/app/study/page.tsx
@@ -1,6 +1,24 @@
 "use client";
 
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  Switch,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useActionState } from "react";
+import { createStudyAction } from "./actions";
+
+export interface CreateStudyState {
+  titleErr?: string;
+  descriptionErr?: string;
+  formErr?: string;
+  success?: boolean;
+}
+
+const initState: CreateStudyState = {};
 
 export default function Study() {
   return (
@@ -14,6 +32,7 @@ export default function Study() {
       >
         <TextField name="title" label="タイトル" />
         <TextField name="description" label="説明" />
+        <FormControlLabel control={<Switch name="isPublic" />} label="公開" />
         <Button type="submit" variant="contained" color="primary">
           作成
         </Button>
